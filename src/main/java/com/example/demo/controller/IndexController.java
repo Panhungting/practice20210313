d052f3121e9efaa9fbd99df9009827dfeac1deb6
Panhungting <a0910827893@gmail.com>
2021-03-13 01:09:32 +0900

    scd commit

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@ResponseBody
+//@Controller
+//@ResponseBody + @Controller = @RestController
+@RestController
+public class IndexController {
+    
+    @RequestMapping("index")
+    public String index() {
+
+        return "hello!??????????????!!!";
+    }
+    
+}
